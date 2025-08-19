c56e33c46b0a5a47c30d7d2ca7d7ce5003618b83
Curry Ryan <RFO7799@hca.corpad.net>
2025-08-19 08:49:58 -0500

    updated secrets logic for google sheets

--- app.py
+++ app.py
@@ -47,7 +47,19 @@ def get_gs_client():
             "Add your Google Service Account JSON (as a dict) under that key."
         )
     try:
-        creds = st.secrets["gcp_service_account"]
+        #creds = st.secrets["gcp_service_account"]
+        creds = {
+        "type": st.secrets.gcp_service_account.type,
+        "project_id": st.secrets.gcp_service_account.project_id,
+        "private_key_id": st.secrets.gcp_service_account.private_key_id,
+        "private_key": st.secrets.gcp_service_account.private_key,
+        "client_email": st.secrets.gcp_service_account.client_email,
+        "client_id": st.secrets.gcp_service_account.client_id,
+        "auth_uri": st.secrets.gcp_service_account.auth_uri,
+        "token_uri": st.secrets.gcp_service_account.token_uri,
+        "auth_provider_x509_cert_url": st.secrets.gcp_service_account.auth_provider_x509_cert_url,
+        "client_x509_cert_url": st.secrets.gcp_service_account.client_x509_cert_url
+        }
         gc = gspread.service_account_from_dict(creds)
         return gc
     except Exception as e:
